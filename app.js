729b8d9b638a01e7566e625cc517ce668718ccdf
Alan Vieceli <alan.vieceli@softplan.com.br>
2019-07-18 15:07:11 -0300

    Usando EJS

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
 var express = require("express");
 var app = express();
 
+app.set("view engine", 'ejs');
+
 app.get("/", function(req, res) {
   res.send("<html><body>Portal de Notícias</body></html>");
 });
 
 app.get("/tecnologia", function(req, res) {
-  res.send("<html><body>Portal de Tecnologia</body></html>");
+  
+  res.render("secao/tecnologia")
 });
 
 
 app.listen(3000, function(){
-  console.log("Sevidor rodando na porta 3000.")
+  console.log("Sevidor rodando na porta 3000. ")
 })
