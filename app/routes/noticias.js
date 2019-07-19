d41f415cd9610ffe803849d869a1e62d9c25389a
Alan Vieceli <alan.vieceli@softplan.com.br>
2019-07-19 13:49:43 -0300

    criando uma view dinamica

--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -9,18 +9,13 @@ module.exports = function (app){
       database: "portal_noticias"
     });
 
-    console.log("OK1")
     connection.query("select * from noticias", function(error, results){      
-      if (error) throw error;
+      if (error) 
+        throw error;
       let string=JSON.stringify(results);
       let json =  JSON.parse(string);
-      console.log('>> json: ', json);
-      
-      //res.render("noticias/noticias")
-      res.json(json)
+      res.render("noticias/noticias", {noticias: results})
+      //res.json(json)
     });
-    
-  
-    
   });
 }
