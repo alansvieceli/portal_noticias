9078ae9bbb8244726cb417e35f269cee1d2241ff
Alan Vieceli <alan.vieceli@softplan.com.br>
2019-07-19 08:24:38 -0300

    Retornando json, acessando mysql

--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -1,5 +1,26 @@
 module.exports = function (app){
+
   app.get("/noticias", function(req, res) {  
-    res.render("noticias/noticias")
+    var mysql = require("mysql");
+    var connection = mysql.createConnection({
+      host: 'localhost',
+      user: 'root',
+      password: "456852",
+      database: "portal_noticias"
+    });
+
+    console.log("OK1")
+    connection.query("select * from noticias", function(error, results){      
+      if (error) throw error;
+      let string=JSON.stringify(results);
+      let json =  JSON.parse(string);
+      console.log('>> json: ', json);
+      
+      //res.render("noticias/noticias")
+      res.json(json)
+    });
+    
+  
+    
   });
 }
