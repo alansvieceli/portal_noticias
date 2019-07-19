1a3decb2280c8bd23d3a00349d5b923aa1581b85
Alan Vieceli <alan.vieceli@softplan.com.br>
2019-07-19 13:56:12 -0300

    Modulando conexão com Mysql

--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -1,14 +1,10 @@
+var dbConnection = require("../../config/dbConnection")
+
 module.exports = function (app){
 
-  app.get("/noticias", function(req, res) {  
-    var mysql = require("mysql");
-    var connection = mysql.createConnection({
-      host: 'localhost',
-      user: 'root',
-      password: "456852",
-      database: "portal_noticias"
-    });
+  var connection = dbConnection();
 
+  app.get("/noticias", function(req, res) {      
     connection.query("select * from noticias", function(error, results){      
       if (error) 
         throw error;
