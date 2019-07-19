9b69a1a7de40fe1008631a6016175b1b56f582a2
Alan Vieceli <alan.vieceli@softplan.com.br>
2019-07-19 15:56:11 -0300

    adicionando body-parser

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -2,4 +2,9 @@ module.exports = function (application) {
   application.get("/formulario_inclusao_noticia", function(req, res) {  
     res.render("admin/form_add_noticia")
   });
+
+  application.post("/noticias/salvar", function(req, res) {  
+    let noticias = req.body;
+    res.send(noticias);
+  });
 }
