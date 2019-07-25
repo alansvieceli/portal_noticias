7d4e3b4b249201ca8b4efa49a9a731fca76c130a
Alan Vieceli <alan.vieceli@softplan.com.br>
2019-07-25 14:13:18 -0300

    add midleware express-validator

--- config/server.js
+++ config/server.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const consign = require("consign");
-const bodyParser = require("body-parser")
+const bodyParser = require("body-parser");
+const expressValidator = require("express-validator");
 
 const app = express();
 app.set("view engine", 'ejs');
 app.set("views", "./app/views"); //a partir de onde o modulo é chamado
 
+//aplicando midlewares
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(expressValidator());
 
 consign()
   .include('app/routes')
