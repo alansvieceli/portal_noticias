6c1f28992575ff4fb5947ddcb50447354502a4b8
Alan Vieceli <alan.vieceli@softplan.com.br>
2019-07-25 15:27:39 -0300

    mantendo preenchimento do formulário

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -1,11 +1,8 @@
-const {
-  check,
-  validationResult
-} = require('express-validator/check');
+const { check, validationResult } = require('express-validator');
 
 module.exports = function (application) {
   application.get("/formulario_inclusao_noticia", function (req, res) {
-    res.render("admin/form_add_noticia")
+    res.render("admin/form_add_noticia", {noticia:{}})
   });
 
   application.post("/noticias/salvar",
@@ -16,16 +13,16 @@ module.exports = function (application) {
     .isLength({min: 10, max: 100})
     .withMessage('Resumo deve ter entre 10 e 100 caracteres'),
     function (req, res) {
-
+      let noticia = req.body;
       const errors = validationResult(req)
       if (!errors.isEmpty()) {
         //return res.status(422).json({errors: errors.array()})
         console.log(errors.array());
-        res.render("admin/form_add_noticia", {validacao: errors.array()});
+        res.render("admin/form_add_noticia", {validacao: errors.array(), noticia: noticia});
         return;
       }
 
-      let noticia = req.body;
+      
       let connection = application.config.dbConnection(); // foi mapeado la no server
       let NoticiasDAO = new application.app.models.NoticiasDAO(connection);
 
